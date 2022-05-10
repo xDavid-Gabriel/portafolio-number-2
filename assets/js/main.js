28175e49c7c8c3b039942be67b5ba924be58de24
xDavid-Gabriel <davidbetalleluz31@gmail.com>
2022-05-10 03:43:15 -0500

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -208,3 +208,33 @@ function navHighlighter() {
   });
 }
 
+// Toggle style switcher
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler")
+styleSwitcherToggle.addEventListener("click",()=>{
+    document.querySelector(".style-switcher").classList.toggle("abrir")
+})
+
+// theme colors
+const alternateStyles = document.querySelectorAll(".alternate-style")
+
+const setActiveStyle = (color)=>{
+    
+alternateStyles.forEach((style)=>{
+if(color === style.getAttribute("title")){
+    style.removeAttribute("disabled")
+}else{
+    style.setAttribute("disabled", "true")
+}
+})
+}
+
+// theme light and dark mode
+
+const dayNight = document.querySelector(".day-night")
+dayNight.addEventListener("click",()=>{
+  dayNight.querySelector("i").classList.toggle("fa-sun")
+    dayNight.querySelector("i").classList.toggle("fa-moon")
+    // dayNight.querySelector("i").classList.toggle("fa-moon")
+
+    document.body.classList.toggle("luz")
+})
